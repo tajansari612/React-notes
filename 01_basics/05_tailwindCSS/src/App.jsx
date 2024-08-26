07fb3e8180013d5438b1c7e554ad1c65e35c9ac6
tajansari612 <tajansari612@gmail.com>
2024-08-26 14:56:48 +0530

    minor change set w-96 to max-w-96 to make it responsive

--- 01_basics/05_tailwindCSS/src/App.jsx
+++ 01_basics/05_tailwindCSS/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1 className="text-4xl font-bold text-center mt-6">
         Adding Tailwind CSS for styling
       </h1>
-      <p className="text-center w-96 mt-8 text-wrap">
+      <p className="text-center max-w-96 mt-8 text-wrap">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, odio
         saepe. Accusantium repellat molestias rerum aut fuga nesciunt unde natus
         minus laborum, dicta incidunt quas eum tempore. Qui, neque et.
