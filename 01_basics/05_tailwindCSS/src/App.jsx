57110696634085d1724443c9ac00bae489303071
tajansari612 <tajansari612@gmail.com>
2024-08-26 14:52:39 +0530

    added tailwind css framework for styling

---
+++ 01_basics/05_tailwindCSS/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center h-screen w-screen bg-blue-400 text-white">
+      <h1 className="text-4xl font-bold text-center mt-6">
+        Adding Tailwind CSS for styling
+      </h1>
+      <p className="text-center w-96 mt-8 text-wrap">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, odio
+        saepe. Accusantium repellat molestias rerum aut fuga nesciunt unde natus
+        minus laborum, dicta incidunt quas eum tempore. Qui, neque et.
+      </p>
+    </div>
+  );
+}
+
+export default App;
