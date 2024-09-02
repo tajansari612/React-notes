acf87de7cd4ca9b87c887ddd7587212adb49fa9a
tajansari612 <tajansari612@gmail.com>
2024-09-02 19:03:06 +0530

    navbar added

--- 01_basics/project_1/components/Navbar.jsx
+++ 01_basics/project_1/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 // Initialization for ES Users
 import { Collapse, initTWE, } from "tw-elements";
@@ -7,112 +6,29 @@ initTWE({ Collapse });
 function Navbar() {
   return (
     <div className='w-screen h-auto'>
-      {/* <!-- Main navigation container --> */}
-      <nav
-        className="relative flex w-full flex-nowrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4"
-        data-twe-navbar-ref>
-        <div className="flex w-full flex-wrap items-center justify-between px-3">
-          <div className="ms-2 md:me-2">
-            <a className="text-xl text-black dark:text-white" href="/">Navbar</a>
-          </div>
-          {/* <!-- Hamburger button for mobile view --> */}
-          <button
-            className="block border-0 bg-transparent px-2 text-black/50 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-            type="button"
-            data-twe-collapse-init
-            data-twe-target="#navbarSupportedContent14"
-            aria-controls="navbarSupportedContent14"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            {/* <!-- Hamburger icon --> */}
-            <span
-              className="[&>svg]:w-7 [&>svg]:stroke-black/50 dark:[&>svg]:stroke-neutral-200">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor">
-                <path
-                  fill-rule="evenodd"
-                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clip-rule="evenodd" />
-              </svg>
-            </span>
-          </button>
+      <div id="navbar" className="bg-white text-black text-lg py-2 px-6 flex flex-row items-center justify-between">
 
-          {/* <!-- Collapsible navbar container --> */}
-          <div
-            className="!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
-            id="navbarSupportedContent14"
-            data-twe-collapse-item>
-            {/* <!-- Left links --> */}
-            <ul
-              className="list-style-none me-auto flex flex-col ps-0 lg:mt-1 lg:flex-row"
-              data-twe-navbar-nav-ref>
-              {/* <!-- Home link --> */}
-              <li
-                className="my-4 ps-2 lg:my-0 lg:pe-1 lg:ps-2"
-                data-twe-nav-item-ref>
-                <a
-                  className="text-black dark:text-white lg:px-2"
-                  aria-current="page"
-                  href="/"
-                  data-twe-nav-link-ref
-                >Home</a
-                >
-              </li>
-              {/* <!-- Link --> */}
-              <li
-                className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0"
-                data-twe-nav-item-ref>
-                <a
-                  className="p-0 text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  href="/"
-                  data-twe-nav-link-ref
-                >About</a>
-              </li>
-            </ul>
-            <div className="w-[300px] lg:pe-2">
-              <div
-                class="relative flex"
-                data-twe-input-wrapper-init
-                data-twe-input-group-ref>
-                <input
-                  type="search"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                  placeholder="Search"
-                  aria-label="Search"
-                  id="exampleFormControlInput"
-                  aria-describedby="basic-addon1" />
-                <label
-                  for="exampleFormControlInput"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-                >Search
-                </label>
-                <button
-                  class="relative z-[2] -ms-0.5 flex items-center rounded-e bg-primary px-5  text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-                  type="button"
-                  id="button-addon1"
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light">
-                  <span class="[&>svg]:h-5 [&>svg]:w-5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-                  </span>
-                </button>
-              </div>
-            </div>
-          </div>
+        {/* brand name and logo */}
+        <div id="brandNameLogo" className="flex flex-row items-center">
+          <span id="logo">
+            <img src="../public/gamingLogo.png" alt=""  width="48px"/>
+          </span>
+          <span id="brandName" className="-translate-x-4">Gaming Hub</span>
         </div>
-      </nav>
+
+        {/* links */}
+        <div id="links" className="flex flex-row gap-6">
+          <a className=" font-semibold text-[17px] hover:text-blue-700 hover:underline underline-offset-4 hover:ease-in-out" href="#home">Home</a>
+          <a className=" font-semibold text-[17px] hover:text-blue-700 hover:underline underline-offset-4" href="#about">About</a>
+          <a className=" font-semibold text-[17px] hover:text-blue-700 hover:underline underline-offset-4" href="#contact">Contact</a>
+        </div>
+
+        {/* search and buton */}
+        <div id="searchAndbutton" className="text-[15px] flex flex-row gap-4">
+          <input className="w-48 border-0 py-[1px] pl-4 rounded-md ring-1 ring-inset ring-blue-300 ring-offset-0 focus:outline-none  focus:ring-2 focus:ring-inset focus:ring-blue-400" placeholder="Quick Search" type="text" name="" id="" />
+          <button className="bg-blue-400 text-white  pb-[2px] pt-[1px] px-4 rounded-md hover:bg-blue-300" type="button">Search</button>
+        </div>
+      </div>
     </div>
   )
 }
