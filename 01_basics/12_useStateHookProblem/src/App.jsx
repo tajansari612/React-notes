039d5a862b4676eb5e4931aef8fed49dcb3e8407
tajansari612 <tajansari612@gmail.com>
2024-09-02 23:59:35 +0530

    new problem solved

---
+++ 01_basics/12_useStateHookProblem/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+function App() {
+
+  // const users = [
+  //   { name: "Taj", age: 24 },
+  //   { name: "Ayaj", age: 22 },
+  //   { name: "Pravin", age: 23 },
+  //   { name: "Shinde", age: 23 }
+  // ]
+
+  const [usersState, setUsersState] = useState([
+    { name: "Taj", age: 24 },
+    { name: "Ayaj", age: 22 },
+    { name: "Pravin", age: 23 },
+    { name: "Shinde", age: 23 }
+  ])
+  return (
+    <div className='flex flex-col items-center h-screen w-screen bg-[#212121] text-white'>
+      <h1 className='text-3xl font-bold text-center my-6'>
+        Hello, This is useState() Hook tutorial!
+      </h1>
+
+      {/* using map or without state */}
+      {/* <ul>
+        {
+          users.map((ele) => {
+            return <li key={ele.index}>{ele.name} is {ele.age} years old!</li>
+          })
+        }
+      </ul> */}
+
+      {/* using state hook */}
+      <ul>
+        {
+          usersState.map((ele) => {
+            return <li key={ele.index}>{ele.name} is {ele.age} years old!</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
