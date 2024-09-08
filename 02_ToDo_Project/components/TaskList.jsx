1529008332474934dcfe944045f566e50523d29f
tajansari612 <tajansari612@gmail.com>
2024-09-08 16:43:31 +0530

    the checked feature is fixed which is lossed on reload

--- 02_ToDo_Project/components/TaskList.jsx
+++ 02_ToDo_Project/components/TaskList.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import { BiCheckCircle } from "react-icons/bi";
 import { MdDeleteForever } from "react-icons/md";
 
@@ -7,14 +6,17 @@ import { MdDeleteForever } from "react-icons/md";
 export default function TaskList(props) {
 
   //handle check function for line-through css
-  const [check, setcheck] = useState(props.currEle.checked)
   const handleCheck = () => {
-    if(check){
-      setcheck(false)
-    }else{
-      setcheck(true)
-    }
+    const updatedTodo = props.todo.map((ele) =>{
+      if(props.currEle === ele){
+        return {...ele, checked:!props.currEle.checked}
+      }else{
+        return ele
+      }
+    })
+    props.settodo(updatedTodo)
   }
+  
   //handle delete task function
   const handleDeleteTask = (value) => {
     // const index = todo.indexOf(value)    //slow
@@ -27,7 +29,7 @@ export default function TaskList(props) {
 
   return (
     <li key={props.currEle} className="bg-yellow-400 mb-4 text-white py-2 px-4 rounded-md flex flex-row justify-end">
-      <span className={`w-full text-wrap ${check ? 'line-through' : 'decoration-none'}`}>{props.currEle.value}</span>
+      <span className={`w-full text-wrap ${props.currEle.checked ? 'line-through' : 'decoration-none'}`}>{props.currEle.value}</span>
       <button id="check-btn" className='mr-2' value={props.currEle.checked} onClick={handleCheck}><BiCheckCircle /></button>
       <button id="delete-btn" onClick={() => (handleDeleteTask(props.currEle))}><MdDeleteForever /></button>
     </li>
